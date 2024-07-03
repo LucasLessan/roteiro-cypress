cb4e9ee26c25ecd8a4149adec70496de5375fe63
LucasLessan <lucasteles2495@gmail.com>
2024-07-02 21:53:41 -0300

    Testes Implementados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,97 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Insere e Completa Tarefas, Depois Limpa a Lista', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('Instalar Kali Linux{enter}')
+      .type('Treinar Vim Motions{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .eq(0)
+      .click();
+
+    cy.get('.todo-list li .toggle')
+      .eq(1)
+      .click();
+
+    cy.contains('Clear completed').click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Completa Tarefa, Depois Desfaz Mudança', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('Instalar LSP{enter}')
+      .type('Redigir Artigo{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .eq(1)
+      .click();
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Redigir Artigo');
+
+    cy.get('.todo-list li .toggle')
+      .eq(0)
+      .click();
+
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2)
+      .eq(1)
+      .should('have.text', 'Redigir Artigo');
+  });
+
+  it('Teste Completo', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('Aprender Arch Linux{enter}')
+      .type('Instalar Arch{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .eq(0)
+      .click();
+
+    cy.get('.todo-list li .toggle')
+      .eq(1)
+      .click();
+
+    cy.get('.new-todo')
+      .type('Configurar Arch{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 3);
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+
+    cy.contains('Clear completed').click();
+
+    cy.get('.new-todo')
+      .type('Configurar DE{enter}')
+      .type('Configurar WM{enter}');
+
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .should('have.length', 3);
+
+    cy.get('.todo-list li .toggle')
+      .eq(0)
+      .click();
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+  });
 });
